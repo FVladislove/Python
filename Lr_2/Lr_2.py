06c3917b6864a2a6353b3720a30aff1bb36d493d
Vlad Finyk <f.vlad.is.love@gmail.com>
2020-02-21 11:02:51 +0200

    continue refactoring

--- Lr_2/Lr_2.py
+++ Lr_2/Lr_2.py
@@ -37,16 +37,16 @@ def move_elements(list: list, num_of_elms, mov_from, mov_to):
     if mov_from >= 0 and mov_to < len(list):
         if mov_from + num_of_elms > len(list):
             to = mov_from - num_of_elms
-            mod_elms_list = list[to:mov_from]
+            mov_elem_list = list[to:mov_from]
             list = list[:to] + list[mov_from:]
-            for j in range(len(mod_elms_list)):
-                list.insert(mov_to + j, mod_elms_list[j])
+            for j in range(len(mov_elem_list)):
+                list.insert(mov_to + j, mov_elem_list[j])
             return list
         to = mov_from + num_of_elms
-        mod_elms_list = list[mov_from:to]
+        mov_elem_list = list[mov_from:to]
         list = list[:mov_from] + list[to:]
-        for j in range(len(mod_elms_list)):
-            list.insert(mov_to, mod_elms_list[j])
+        for j in range(len(mov_elem_list)):
+            list.insert(mov_to, mov_elem_list[j])
             mov_to = mov_to + 1
     return list
 
@@ -82,30 +82,42 @@ list2 += list2_copy
 
 print("List 2 + list 2 copy  = ", list2)
 print("Zero element of list 2  =", list2[0])
+
 list1 += 3, 5, 6
 list2[1] = list1
+
 print("1 element of list 2 changed to list 1  = ", list2)
+
 list2.remove(list2[2])
 print("Remove 2 element from list 2  = ", list2)
-print("Max element of list  = ", max(list1))
+
+print("Max element of list 1 = ", max(list1))
+
 i = [22, ['ogo', 3, 88], 'apple']
 print("This is 88  - ", i[1][2])
+
 list3 = list2[int(len(list2) / 2):]
-print(list2)
+print("list 3 = " + str(list3))
+
 print(move_elements(list2, 3, 5, 1))
+
 n_sorted_list = [5, 6, 7, 7, 5, 43, 2, 3, 5, 6, 7, 9, 9, 9, 7, 43, 2, 0]
+
 sorted_list = selection_sort(n_sorted_list)
-print(f"sorted list = {selection_sort(sorted_list)}")
+print(f"sorted list = {sorted_list}")
 '''
 for i in range(len(sorted_list)):
     print(sorted_list[i])
 '''
 for i in sorted_list:
     print(i, end=' ')
+
 list_sum = 0
 for i in sorted_list:
     list_sum = list_sum + i
 print(f"\nlist sum = {list_sum}")
+
 hello_list = 'Hello'
+
 hello_3 = [symb * 3 for symb in hello_list]
 print(hello_3)
